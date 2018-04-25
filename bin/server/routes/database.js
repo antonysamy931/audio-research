14adc58854dd247a612e40ee6eb5c67257e05b51
root <antonysamyjoseph1988@gmail.com>
2018-04-25 09:54:26 +0530

    Sqlite research while application start

--- bin/server/routes/database.js
+++ bin/server/routes/database.js
@@ -1,12 +1,58 @@
 const express = require('express');
+const path = require('path');
 const sqlite = require('sqlite3').verbose();
 
-let db = new sqlite.Database('./bin/server/db/Audio_Player.db', sqlite.OPEN_READWRITE, (err) => {
+const databasePath = path.join(__dirname ,"../db/Audio_Player.db");
+console.log(databasePath);
+
+let db = new sqlite.Database(databasePath, (err) => {
     if(err){
         console.log(err);
     }
-    console.log('SQLite database connected');    
+    //console.log('SQLite database connected');    
 });
+
+db.serialize(() =>{
+    db.run(`CREATE TABLE IF NOT EXISTS Users(
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    Name TEXT NOT NULL,
+    UserName TEXT NOT NULL,
+    Password TEXT NOT NULL,
+    Role TEXT NOT NULL)`)
+    .run(`CREATE TABLE IF NOT EXISTS PlayList(
+        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+        Name TEXT NOT NULL,
+        UserId INTEGER NOT NULL,
+        FOREIGN KEY(UserId) REFERENCES Users(Id)
+    )`);
+});
+
+db.parallelize(() => {
+    InsertRecord(db);
+});
+
+function InsertRecord(db){
+    let query = "SELECT COUNT('') AS Count FROM Users WHERE UserName = ?";
+    let param = "admin";
+    const insertQuery = `INSERT INTO Users VALUES (1,"Admin","admin","admin","admin")`;
+    count = 0;
+    db.each(query,[param],(err, row)=>{
+        if(err){
+            console.log(err);
+        }
+        count = row.Count;        
+    });
+
+    if(count == 0){
+        console.log(count);
+        db.run(insertQuery,(err) => {
+            if(err){
+                console.log(err);
+            }
+            console.log(this.lastID);
+        });
+    }
+}
 
 db.close((err) => {
     if(err){
